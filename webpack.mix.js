5f6b120f7c401a23e49a1111d8289d4d42c72d4a
Alyxia Sother <alyxia@riseup.net>
2022-02-09 21:06:04 +0100

    [Mix] Do the necessary preparations

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass("resources/sass/app.scss", "public/css")
+    .copy(
+        "resources/images/default-avatar.png",
+        "storage/app/public/uploads/avatars/default-avatar.png"
+    )
+    .sourceMaps();
