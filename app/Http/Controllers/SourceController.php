94067caa9d084756194edfe0dd9fa91464a8ed7b
Alyxia Sother <alyxia@riseup.net>
2022-02-10 21:16:43 +0100

    [SourceController] Implement fetching data

--- app/Http/Controllers/SourceController.php
+++ app/Http/Controllers/SourceController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class SourceController extends Controller
 {
     public function dump()
     {
-        return view('source', ['sourceData' => null]);
+        // TODO: Please, for the love of God, define a data structure for the response as a class.
+        $res = $this->getSourceData('https://cumcordplugins.github.io/Condom/plugins-large.json');
+        return view('source', ['sourceData' => $res]);
     }
 
     public function aly()
     {
         return view('source', ['sourceData' => null]);
+    }
+
+    /**
+     * Get the JSON data for a source.
+     * @param string $sourceUrl
+     * @return mixed
+     */
+    public function getSourceData($sourceUrl)
+    {
+        $sourceData = null;
+        return Http::get($sourceUrl)->json();
     }
 }
