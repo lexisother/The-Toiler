8872ecdf13f1c430c05b2d3ed745acf2a15c89e9
Alyxia Sother <alyxia@riseup.net>
2022-02-10 21:16:45 +0100

    [views/source] Display data

--- resources/views/source.blade.php
+++ resources/views/source.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
     @if ($sourceData)
-
+        @foreach ($sourceData as $plugin)
+            <p>
+                {{ $plugin["name"] }}
+            </p>
+        @endforeach
     @else
         <h1>Something went wrong. No data was received.</h1>
     @endif
